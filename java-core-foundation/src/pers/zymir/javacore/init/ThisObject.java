1dc42545e8b3794cdd608b9baa31cec53537f5f7
李子木 <zimu.li@lofty.com>
2025-05-17 23:46:20 +0800

    feat: this关键字说明

--- java-core-foundation/src/pers/zymir/javacore/init/ThisObject.java
+++ java-core-foundation/src/pers/zymir/javacore/init/ThisObject.java
@@ -4,11 +4,11 @@ public class ThisObject {
 
   public static void main(String[] args) {
     ThisObject thisObject = new ThisObject();
-    thisObject.thisFoo();
+    thisObject.thisFoo(100);
   }
 
   // this被编译器隐式传递到实例方法的第一个参数，可以显式的声明出来证明这一点
-  public void thisFoo(ThisObject this) {
-    System.out.println("thisFoo");
+  public void thisFoo(ThisObject this, int value) {
+    System.out.println("thisFoo" + value);
   }
 }
