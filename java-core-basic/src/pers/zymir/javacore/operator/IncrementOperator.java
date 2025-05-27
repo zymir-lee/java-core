27fdaf7c93f77558f6363513194ee086b3bbfb57
zymir-li <amormz@foxmail.com>
2025-05-27 23:40:45 +0800

    feat: 操作符 - 防止自增/减操作符滥用

--- java-core-basic/src/pers/zymir/javacore/operator/IncrementOperator.java
+++ java-core-basic/src/pers/zymir/javacore/operator/IncrementOperator.java
@@ -5,11 +5,15 @@ public class IncrementOperator {
 
     // 后缀：先得到返回值再进行计算
     int value = 1;
-    System.out.println(value++);
-    System.out.println(value);
+    System.out.println(value++); // 1
+    System.out.println(value); // 2
 
     // 前缀：先进行计算在得到返回值
     int value02 = 1;
-    System.out.println(++value02);
+    System.out.println(++value02); // 2
+
+    // 不要滥用自增操作符 不要写出下面这样逆天的代码
+    int i =1;
+    System.out.println(i++ + ++i);
   }
 }
